b5d0ed03d049e6561bf64c9476d557bd6f8a8038
proDream <sushkoos@gmail.com>
2025-03-12 06:14:36 +0400

    feat(src/utils/event_message_utils.rs): handle pull request events
    - added support for generating messages for PullRequestEvent
    - updated generate_push_message to handle PushEvent
    - renamed functions and variables to be more descriptive

--- src/utils/event_message_utils.rs
+++ src/utils/event_message_utils.rs
@@ -1,15 +1,36 @@
 use crate::structures::data_structure::DataStructure;
-use crate::structures::event_structures::PushEvent;
+use crate::structures::event_structures::{PullRequestEvent, PushEvent};
 use crate::utils::text_utils::{
-    generate_input_message, generate_notify_fields, get_input_title,
+    generate_input_message, generate_pull_request_notify_fields, generate_push_notify_fields,
+    get_pull_request_input_title, get_pull_request_title, get_push_input_title,
 };
 
 pub fn generate_push_message(data: &DataStructure, event: &PushEvent) -> String {
+    let mut message: String = "".to_owned();
+
+    message += &*get_push_input_title(data.title.as_deref().unwrap_or_default(), &data.status);
+
+    message += &*generate_push_notify_fields(data, &event);
+
+    message += &*generate_input_message(data.message.as_deref().unwrap_or_default());
+
+    message += &*generate_input_message(data.footer.as_deref().unwrap_or_default());
+
+    message
+}
+
+pub fn generate_pull_request_message(data: &DataStructure, event: &PullRequestEvent) -> String {
     let mut message = "".to_owned();
 
-    message += &*get_input_title(data.title.as_deref().unwrap_or_default(), &data.status);
+    message += &*get_pull_request_input_title(
+        data.title.as_deref().unwrap_or_default(),
+        &event.action,
+        &event.number,
+    );
+    
+    message += &*get_pull_request_title(&event.pull_request);
 
-    message += &*generate_notify_fields(data, &event);
+    message += &*generate_pull_request_notify_fields(data, &event);
 
     message += &*generate_input_message(data.message.as_deref().unwrap_or_default());
 
